67795e32a148c1306a62989be33663a0fc93e74d
enter <enter@DESKTOP-O16R8JK>
2018-03-12 23:12:00 +0800

    1

---
+++ AutoProject/AutoHuobi/HbAnalyzeApi.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoHuobi
+{
+    public class HbAnalyzeApi
+    {
+        public ResponseKline kline(string symbol, string period, int size = 300)
+        {
+            var url = $"{HbConfig.baseUrl}/history/kline";
+            url += $"?symbol={symbol}&period={period}&size={size}";
+
+            int httpCode = 0;
+            var result = HttpUtils.RequestDataSync(url, "GET", null, null, out httpCode);
+            return JsonConvert.DeserializeObject<ResponseKline>(result);
+        }
+    }
+}
---
+++ AutoProject/AutoHuobi/HbConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoHuobi
+{
+    public class HbConfig
+    {
+        public static string domain = "api.huobipro.com/market";// "be.huobi.com";
+        public static string baseUrl = $"https://{domain}";
+    }
+}
